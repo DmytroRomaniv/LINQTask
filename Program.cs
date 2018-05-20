ed2ddce78960b24c98f25922b95cee2808c914da
DmytroRomaniv <d.romanivvv@gmail.com>
2018-05-20 21:48:38 +0200

    Added Group1 Tasks

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileReader.ReadFile("data.txt");
+        }
+    }
+}
